fa303428b93b627a9d6844c4724ab2da7424b1ca
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-09-13 14:53:42 +0300

    Allow to interpret property reference

--- compiler/ir/ir.interpreter/src/org/jetbrains/kotlin/ir/interpreter/checker/IrCompileTimeChecker.kt
+++ compiler/ir/ir.interpreter/src/org/jetbrains/kotlin/ir/interpreter/checker/IrCompileTimeChecker.kt
@@ -239,4 +239,8 @@ class IrCompileTimeChecker(
     override fun visitThrow(expression: IrThrow, data: Nothing?): Boolean {
         return expression.value.accept(this, data)
     }
+
+    override fun visitPropertyReference(expression: IrPropertyReference, data: Nothing?): Boolean {
+        return mode.canEvaluateFunction(expression.getter!!.owner)
+    }
 }
