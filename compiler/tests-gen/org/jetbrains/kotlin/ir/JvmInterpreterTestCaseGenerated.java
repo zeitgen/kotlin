187e89cd582bc764c5677f8d51beff737e53156c
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-09-20 23:07:10 +0300

    Create test file to check class reference interpretation

--- compiler/tests-gen/org/jetbrains/kotlin/ir/JvmInterpreterTestCaseGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/ir/JvmInterpreterTestCaseGenerated.java
@@ -515,6 +515,11 @@ public class JvmInterpreterTestCaseGenerated extends AbstractJvmInterpreterTestC
             KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/ir/interpreter/jvm"), Pattern.compile("^(.+)\\.kt(s)?$"), null, true);
         }
 
+        @TestMetadata("classReference.kt")
+        public void testClassReference() throws Exception {
+            runTest("compiler/testData/ir/interpreter/jvm/classReference.kt");
+        }
+
         @TestMetadata("javaStatic.kt")
         public void testJavaStatic() throws Exception {
             runTest("compiler/testData/ir/interpreter/jvm/javaStatic.kt");
