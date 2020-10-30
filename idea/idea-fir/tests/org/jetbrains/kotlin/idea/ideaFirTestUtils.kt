d223791813603bfce825e10209a9f5edea21bb7e
Roman Golyshev <roman.golyshev@jetbrains.com>
2020-10-30 17:14:39 +0300

    FIR IDE: Refactor `AbstractFirShortenRefsTest`
    - Use `FIR_COMPARISON` directive
    - Move testing utils to `ideaFirTestUtils.kt`
    - Ignore `TopLevelFunctionImportWithLotsOfFqName.kt` test for now (it
    takes too long to execute)

--- idea/idea-fir/tests/org/jetbrains/kotlin/idea/ideaFirTestUtils.kt
+++ idea/idea-fir/tests/org/jetbrains/kotlin/idea/ideaFirTestUtils.kt
@@ -17,10 +17,48 @@ import org.jetbrains.kotlin.idea.frontend.api.fir.KtFirAnalysisSessionProvider
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.trackers.KotlinOutOfBlockModificationTrackerFactory
+import org.jetbrains.kotlin.test.InTextDirectivesUtils
+import java.io.File
 
 fun Throwable.shouldBeRethrown(): Boolean = when (this) {
     is DuplicatedFirSourceElementsException -> true
     else -> false
+}
+
+const val FIR_IGNORE = "// FIR_IGNORE"
+const val FIR_COMPARISON = "// FIR_COMPARISON"
+
+/**
+ * Set this flag to `true` to insert directive automatically to all files
+ * that pass tests but do not already have the directive.
+ */
+private const val insertDirectiveAutomatically = false
+
+fun runTestWithCustomEnableDirective(directive: String, testFile: File, test: () -> Unit) {
+    val testFileAfter = testFile.resolveSibling(testFile.name + ".after").takeIf { it.exists() }
+
+    val testEnabled = InTextDirectivesUtils.isDirectiveDefined(testFile.readText(), directive)
+
+    try {
+        test()
+    } catch (e: Throwable) {
+        if (testEnabled) throw e
+        return
+    }
+
+    if (!testEnabled) {
+        if (insertDirectiveAutomatically) {
+            testFile.insertDirective(directive)
+            testFileAfter?.insertDirective(directive)
+        }
+
+        throw AssertionError("Looks like test is passing, please add ${directive.removePrefix("// ")} at the beginning of the file")
+    }
+}
+
+private fun File.insertDirective(directive: String) {
+    val originalText = readText()
+    writeText("$directive\n$originalText")
 }
 
 @OptIn(InvalidWayOfUsingAnalysisSession::class)
