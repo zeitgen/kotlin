4312c924c8ef8c51e9482587012fa06aeeffb3a0
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-01-29 18:39:30 +0300

    FIR: Fix uninitialized lateinit FirSymbol::symbol for supertype of Java class
    Earlier, isForSupertypes has been mapped to different boolean parameter

---
+++ compiler/testData/diagnostics/tests/j+k/supertypeUsesNested.kt
@@ -0,0 +1,22 @@
+// FIR_IDENTICAL
+// FILE: JavaClass.java
+public class JavaClass extends ContainerType<Container<JavaClass.Nested>> {
+    public static class Nested extends Container<String> {}
+}
+
+// FILE: ContainerType.java
+public class ContainerType<T> {}
+
+// FILE: Container.java
+public class Container<K> {}
+
+// FILE: Usage.java
+public class Usage {
+    public static JavaClass.Nested foo() { return null; }
+}
+
+// FILE: main.kt
+
+fun main() {
+    Usage.foo().hashCode()
+}
