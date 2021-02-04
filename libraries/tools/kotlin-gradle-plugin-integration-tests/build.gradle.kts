4d2da279bd05a95861152758f7ab71465472367f
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2021-02-04 14:54:57 +0100

    Update Gradle version for tests to 6.8.1.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/build.gradle.kts
@@ -78,7 +78,7 @@ fun Test.includeNative(include: Boolean) {
 }
 
 fun Test.advanceGradleVersion() {
-    val gradleVersionForTests = "6.3"
+    val gradleVersionForTests = "6.8.1"
     systemProperty("kotlin.gradle.version.for.tests", gradleVersionForTests)
 }
 
