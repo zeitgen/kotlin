010b1e32f3fcf14f871aa050ffc96d025ba66368
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2021-02-09 12:42:08 +0100

    ~mute rerun

--- gradle/testRetry.gradle.kts
+++ gradle/testRetry.gradle.kts
@@ -3,7 +3,7 @@ apply(plugin = "org.gradle.test-retry")
 
 tasks.withType<Test>().configureEach {
     configure<org.gradle.testretry.TestRetryTaskExtension> {
-        maxRetries.set(if (kotlinBuildProperties.isTeamcityBuild) 3 else 0)
+        maxRetries.set(if (kotlinBuildProperties.isTeamcityBuild) 0 else 0)
         maxFailures.set(20)
         failOnPassedAfterRetry.set(false)
     }
